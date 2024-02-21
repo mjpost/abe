6449b735e26ce4163c7a5fcaf245b3116bb2645b
Matt Post <post@cs.jhu.edu>
2024-02-21 08:04:25 -0500

    Parameterize generate script

--- generate.py
+++ generate.py
@@ -3,17 +3,32 @@
 import sys
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M")
-model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M")
+def main(args):
+    # m2m100_1.2B
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(args.model_name)
 
-for line in sys.stdin:
-    article = line.rstrip()
-    inputs = tokenizer(article, return_tensors="pt")
+    for line in sys.stdin:
+        line = line.rstrip()
+        inputs = tokenizer(line, return_tensors="pt")
 
-    translated_tokens = model.generate(
-        **inputs, forced_bos_token_id=tokenizer.lang_code_to_id["fra_Latn"], max_length=30
-    )
+        translated_tokens = model.generate(
+            **inputs, forced_bos_token_id=tokenizer.lang_code_to_id[args.target_lang], max_length=args.max_output_tokens
+        )
 
-    result = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+        result = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
 
-    print(translated_tokens)
+        print(result)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-name", "-m", type=str, default="facebook/m2m100_418M", help="Model name")
+    parser.add_argument("--target-lang", "-t", type=str, default="fr", help="Target language")
+    parser.add_argument("--num-beams", "-b", type=int, default=5, help="Number of beams for beam search")
+    parser.add_argument("--noise", "-n", type=float, default=None, help="Add noise to final model logits")
+    parser.add_argument("--max-output-tokens", "-l", type=int, default=30, help="Maximum number of output tokens")
+    args = parser.parse_args()
+
+    main(args)
