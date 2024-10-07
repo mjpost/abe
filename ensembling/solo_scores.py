b1e89a6bb04325156e7faa799b0113ddc2f41b6c
Rachel Wicks <rachel@wicks.me>
2024-10-07 15:37:10 -0400

    rewrite solo scores so you can call function instead of only cli

--- ensembling/solo_scores.py
+++ ensembling/solo_scores.py
@@ -10,15 +10,20 @@ def load_model(model_name, device):
     model = AutoModelWithLMHead.from_pretrained(model_name)
     return model.eval().to(device)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", "-m", type=str, default="facebook/nllb-200-distilled-600M")
-    parser.add_argument("--input", "-i", default=None, type=str)
-    parser.add_argument("--output", "-o", default=None, type=str)
-    parser.add_argument("--cpu", default=False, action="store_true")
+def get_scores(line, model, device=torch.device('cpu')):
+    encoder_input = torch.tensor([line["inputs"]]).to(device)
+    decoder_input = torch.tensor([line["outputs"]]).to(device)
 
-    args = parser.parse_args()
+    outputs = model(input_ids=encoder_input, decoder_input_ids=decoder_input).logits[0]
+    outputs = torch.nn.functional.log_softmax(outputs, dim=-1)
 
+    token_scores = []
+    for id, logits in zip(decoder_input[0][1:], outputs[:-1]):
+        token_scores.append(logits[id].item())
+
+    return token_scores
+
+def main(args):
     device = torch.device('cuda') if torch.cuda.is_available() and not args.cpu else torch.device('cpu')
 
     model = load_model(args.model_name, device)
@@ -29,14 +34,19 @@ if __name__ == "__main__":
     for line in istream:
         line = json.loads(line.strip())
 
-        encoder_input = torch.tensor([line["inputs"]]).to(device)
-        decoder_input = torch.tensor([line["outputs"]]).to(device)
+        token_scores = get_scores(line, model, device)
 
-        outputs = model(input_ids=encoder_input, decoder_input_ids=decoder_input).logits[0]
-        outputs = torch.nn.functional.log_softmax(outputs, dim=-1)
+        print(json.dumps({"scores": token_scores}), file=ostream)
 
-        token_scores = []
-        for id, logits in zip(decoder_input[0][1:], outputs[:-1]):
-            token_scores.append(logits[id].item())
-        print(json.dumps({"scores": token_scores }), file=ostream)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", "-m", type=str, default="facebook/nllb-200-distilled-600M")
+    parser.add_argument("--input", "-i", default=None, type=str)
+    parser.add_argument("--output", "-o", default=None, type=str)
+    parser.add_argument("--cpu", default=False, action="store_true")
+
+    args = parser.parse_args()
+
+    main(args)
+
 
